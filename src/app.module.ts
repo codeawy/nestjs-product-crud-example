257f3ab2c5d5089eb9b56fa992a11af85ceca4ea
codeawy <codeawy@gmail.com>
2025-10-01 19:13:52 +0300

    - Add ProductsModule to app.module.ts imports - Create product interface

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule],
   controllers: [],
   providers: [AppService],
 })
